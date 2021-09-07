1ae941533587df4c8e769f05b70fcccd6529dec1
Jean Michel Marques Cabral <jean.cabral@ibm.com>
2021-09-07 18:55:29 -0300

    first commit

---
+++ src/main/java/com/texo/pioresfilmes/PioresFilmesApplication.java
@@ -0,0 +1,13 @@
+package com.texo.pioresfilmes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PioresFilmesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PioresFilmesApplication.class, args);
+	}
+
+}
